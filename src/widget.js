7323a4c3b9978b1e8fc58fe749e6cc83045af44f
Stephen Beckett <2587374+stephenbeckett@users.noreply.github.com>
2020-12-30 17:35:38 +0000

    re: version 0.0.4

--- src/widget.js
+++ src/widget.js
@@ -3,17 +3,4 @@ import './lib/js/app'
 import './lib/js/chunk-vendors'
 import './lib/css/app.css'
 import './lib/css/chunk-vendors.css'
-
-var head = document.getElementsByTagName('head')[0];
-
-function add(filename){
-  var inject = document.createElement('link');
-  inject.href = filename;
-  inject.type = "text/css";
-  inject.rel = "stylesheet";
-  head.append(inject);
-}
-
-[
-  "https://fonts.googleapis.com/css?family=Roboto:400,500"
-].map(add);
+import './fonts.css'
