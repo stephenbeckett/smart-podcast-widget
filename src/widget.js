fabd434a0bd088a40213cbd384a0ef17ff436f0f
Stephen Beckett <2587374+stephenbeckett@users.noreply.github.com>
2020-12-30 10:11:34 +0000

    re: version 0.0.3

--- src/widget.js
+++ src/widget.js
@@ -1,21 +1,19 @@
 
 import './lib/js/app'
 import './lib/js/chunk-vendors'
-import './css/app.css'
-import './css/chunk-vendors.css'
+import './lib/css/app.css'
+import './lib/css/chunk-vendors.css'
 
 var head = document.getElementsByTagName('head')[0];
 
 function add(filename){
-  var isCss = (filename.includes("css"));
-  var inject = document.createElement((isCss) ? 'link' : 'script');
-  inject[(isCss) ? 'href' : 'src'] = filename;
-  inject.type = "text/" + ((isCss) ? 'css' : 'javascript');
-  if (isCss) inject.rel = "stylesheet";
+  var inject = document.createElement('link');
+  inject.href = filename;
+  inject.type = "text/css";
+  inject.rel = "stylesheet";
   head.append(inject);
 }
 
 [
-  "https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900", 
-  "https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css",
+  "https://fonts.googleapis.com/css?family=Roboto:400,500"
 ].map(add);
